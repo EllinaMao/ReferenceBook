360653b06044e6efdcdce17587968026bbcac116
Olga <xotaoshi@gmail.com>
2025-05-30 03:19:05 +0300

    finished find

--- ReferenceBook/ReferenceBookFind.cpp
+++ ReferenceBook/ReferenceBookFind.cpp
@@ -21,7 +21,7 @@ vector<const RFPage*> ReferenceBookFind::findByName(const vector<RFPage>& pages,
 vector<const RFPage*> ReferenceBookFind::findByOwner(const vector<RFPage>& pages, const string& owner)
 {
     if (owner.empty()) {
-        throw invalid_argument("Name cannot be empty");
+        throw invalid_argument("owner cannot be empty");
     }
     if (pages.empty()) {
         throw invalid_argument("Pages cannot be empty");
@@ -30,6 +30,42 @@ vector<const RFPage*> ReferenceBookFind::findByOwner(const vector<RFPage>& pages
     vector<const RFPage*> result;
     for (auto it = pages.cbegin(); it != pages.cend(); ++it) {
         if (it->getOwner() == owner) {
+            result.push_back(&(*it));
+        }
+    }
+    return result;
+}
+
+vector<const RFPage*> ReferenceBookFind::findByPhone(const vector<RFPage>& pages, const string& phone)
+{
+    if (phone.empty()) {
+        throw invalid_argument("phone cannot be empty");
+    }
+    if (pages.empty()) {
+        throw invalid_argument("Pages cannot be empty");
+    }
+
+    vector<const RFPage*> result;
+    for (auto it = pages.cbegin(); it != pages.cend(); ++it) {
+        if (it->getPhone() == phone) {
+            result.push_back(&(*it));
+        }
+    }
+    return result;
+}
+
+vector<const RFPage*> ReferenceBookFind::findByActivity(const vector<RFPage>& pages, const string& activity)
+{
+    if (activity.empty()) {
+        throw invalid_argument("activity cannot be empty");
+    }
+    if (pages.empty()) {
+        throw invalid_argument("Pages cannot be empty");
+    }
+
+    vector<const RFPage*> result;
+    for (auto it = pages.cbegin(); it != pages.cend(); ++it) {
+        if (it->getActivityType() == activity) {
             result.push_back(&(*it));
         }
     }
